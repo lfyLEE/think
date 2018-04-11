2622068ab2f172489eb88813a3a55521d35cf877
lify <lify@youshu.cc>
2018-04-11 21:38:12 +0800

    init

---
+++ application/index/controller/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lify
+ * Date: 2018/4/11
+ * Time: 下午9:15
+ */
+
+namespace app\index\controller;
+
+
+class Test
+{
+    public function ttt() {
+        echo 'hello';
+    }
+}
