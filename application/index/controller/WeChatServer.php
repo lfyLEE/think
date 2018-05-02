39d1f2d8642721bdc61f75654f71bea0f87d8ac2
lify <lify@youshu.cc>
2018-05-02 10:41:24 +0800

    test

--- application/index/controller/WeChatServer.php
+++ application/index/controller/WeChatServer.php
@@ -19,11 +19,11 @@ class WeChatServer
     {
         $client = new Client([
             // Base URI is used with relative requests
-            'base_uri' => 'http://www.readwith.com',
+            'base_uri' => 'http://readooapi.youshu.cc',
             // You can set any number of default request options.
             'timeout'  => 2.0,
         ]);
-        $result = $client->request('GET', '/Test/test');
+        $result = $client->request('GET', '/Wxpay/getPlanInfo?plan_id=66');
         dump($result);
     }
 }
