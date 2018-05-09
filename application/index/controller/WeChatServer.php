c97527ec94211ff679e346ea370a73e7c1215020
lify <lify@youshu.cc>
2018-05-09 12:01:07 +0800

    test

--- application/index/controller/WeChatServer.php
+++ application/index/controller/WeChatServer.php
@@ -21,7 +21,7 @@ class WeChatServer
      */
     public function run()
     {
-        Log::write(json_encode($_GET));
+        Log::write(json_encode($_GET), 'debug');
         $config = [
             'app_id' => 'wx14c234c622a85b21',
             'secret' => '7a7cb78c2d86044fad4de7192bacd1aa',
