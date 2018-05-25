3cd7598afdf9f60e89672c419b78a80aa99c5065
lify <lify@youshu.cc>
2018-05-25 20:33:50 +0800

    test

--- application/service/WeChat.php
+++ application/service/WeChat.php
@@ -44,34 +44,101 @@ class WeChat
     {
         switch ($message['MsgType']) {
             case 'event':
-                return '收到事件消息';
+                $message['event'] = strtolower($message['event']);
+                switch ($message['event']) {
+                //关注
+                    case 'subscribe':
+                        //二维码关注
+                        if(isset($request['eventkey']) && isset($request['ticket'])){
+                            $data = '二维码关注';
+                            //普通关注
+                        }else{
+                            $data = '普通关注';
+                        }
+                        break;
+                    //扫描二维码(已关注)
+                    case 'scan':
+                        $data = '扫码二维码';
+                        break;
+                    //地理位置
+                    case 'location':
+                        $data = '地理位置';
+                        break;
+                    //自定义菜单 - 点击菜单拉取消息时的事件推送
+                    case 'click':
+                        $data = 'click';
+                        break;
+                    //自定义菜单 - 点击菜单跳转链接时的事件推送
+                    case 'view':
+                        $data = 'view';
+                        break;
+                    //自定义菜单 - 扫码推事件的事件推送
+                    case 'scancode_push':
+                        $data = 'scancode_push';
+                        break;
+                    //自定义菜单 - 扫码推事件且弹出“消息接收中”提示框的事件推送
+                    case 'scancode_waitmsg':
+                        $data = 'scancode_waitmsg';
+                        break;
+                    //自定义菜单 - 弹出系统拍照发图的事件推送
+                    case 'pic_sysphoto':
+                        $data = 'pic_sysphoto';
+                        break;
+                    //自定义菜单 - 弹出拍照或者相册发图的事件推送
+                    case 'pic_photo_or_album':
+                        $data = 'pic_photo_or_album';
+                        break;
+                    //自定义菜单 - 弹出微信相册发图器的事件推送
+                    case 'pic_weixin':
+                        $data = 'pic_weixin';
+                        break;
+                    //自定义菜单 - 弹出地理位置选择器的事件推送
+                    case 'location_select':
+                        $data = 'location_select';
+                        break;
+                    //取消关注
+                    case 'unsubscribe':
+                        $data = 'unsubscribe';
+                        break;
+                    //群发接口完成后推送的结果
+                    case 'masssendjobfinish':
+                        $data = 'masssendjobfinish';
+                        break;
+                    //模板消息完成后推送的结果
+                    case 'templatesendjobfinish':
+                        $data = 'templatesendjobfinish';
+                        break;
+                    default:
+                        $data = '';
+                        break;
+                }
                 break;
             case 'text':
-                $this->send($message['FromUserName'], 'wV9TgcdC10jTy5LwfvybqHPitsu4NQVuUbRk7jyyPaA');
-                return json_encode($message);
+                $data = json_encode($message);
                 break;
             case 'image':
-                return '收到图片消息';
+                $data = '收到图片消息';
                 break;
             case 'voice':
-                return '收到语音消息';
+                $data = '收到语音消息';
                 break;
             case 'video':
-                return '收到视频消息';
+                $data = '收到视频消息';
                 break;
             case 'location':
-                return '收到坐标消息';
+                $data = '收到坐标消息';
                 break;
             case 'link':
-                return '收到链接消息';
+                $data = '收到链接消息';
                 break;
             case 'file':
-                return '收到文件消息';
+                $data = '收到文件消息';
                 break;
             default:
-                return '收到其它消息';
+                $data = '收到其它消息';
                 break;
         }
+        return $data;
     }
 
     /**
